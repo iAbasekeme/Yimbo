940523cff1cec80f8f65142812c747676acbb5f2
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-13 18:05:26 +0100

    Added doc

--- models/users.py
+++ models/users.py
@@ -1,9 +1,11 @@
+'''A user model'''
+from . import Base
 from sqlalchemy import create_engine, Column, Integer, String, Date,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
 class User(Base):
+    '''A user class'''
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     first_name = Column(String(60), nullable=False, unique=True)
