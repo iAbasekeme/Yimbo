87c12e0f55d04bcea593467864bc3c3fb53b305c
elpastore <abdoulayesadio1997@gmail.com>
2024-03-13 17:18:44 +0000

    templates

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-app.route('/home')
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+@app.route('/home')
 def home_page():
     """
     route for the landing page define by wisdom
     """
-    return render_template('landin_page.html')
+    return render_template('home_page.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
