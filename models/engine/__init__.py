75ac8d083475fe1742becd7993ce4fb3bbb3101f
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-14 20:30:20 +0100

    updated the init file to use flask sqlalchemy

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,2 +1,12 @@
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    '''Subclass for all classes'''
+    pass
+
+
+'''db object using the SQLAlchemy constructor.'''
+db = SQLAlchemy(model_class=Base)
