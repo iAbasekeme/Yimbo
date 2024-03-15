65b9667a5787aae8672301dd322652794a43ccdd
wishon1 <wisdomhonest1@gmail.com>
2024-03-15 14:48:22 -0400

    podcast

--- podcast_fetch.py
+++ podcast_fetch.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+Usage:
+    ./14-model_city_fetch_by_state.py <mysql_username> <mysql_password>
+    <database_name>
+"""
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from podcast_model.py import PodcastCategory, Podcast
+from podcast_model.py import Category, Podcast, Country, Region
+
 
 
 if __name__ == "__main__":
@@ -12,7 +18,49 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for podcast, podcast_cat in session.query(Podcast, Category) \
-                              .filter(Podcast.category_id == Category.id) \
-                              .order_by(Podcast.id):
-        print("{}: ({}) {}".format(Category.name, Podcast.id, Podcast.name))
+    def print_by_podcast():    
+        """
+        Prints all available podcasts and their descriptions.
+        """
+
+        try:
+            available_podcasts = session.query(Podcast).all()
+            print("Available Podcasts:")
+
+            for podcast in available_podcasts:
+                print(f"{podcast.name} : [description: {podcast.description}]")
+        except Exception as e:
+            print(f"An error occurred while querying podcasts: {e}")
+
+    def print_by_category(category_name)
+        """ this method prints podcast based on the category given"""
+        # query the category table
+        try:
+            category = sesion.query(category).filter_by(
+                       name=category_name).first()
+            
+            if category is None:
+                print("category: {} not found".format(category.name))
+                return
+            
+            cat_id = category.id
+            category_podcast = ssession.query(Podcast).filter_by(
+                               category_id=cat_id).all()
+
+            if category_podcast is None:
+                print("No podcast found in category {}".format(
+                       category.name) end=,;)
+                return
+            print("list of podcast in {}:".format(category.name))
+            for podcast in category_podcast:
+                print(podcast.name)
+        except Exception as e:
+            print("An error occured while trying to query the database")
+
+
+if __name__ == "__main__":
+    try:
+        print_by_podcast()  # Example usage
+        print_by_category("Tech & Science")  # Example usage with category
+    except Exception as e:
+        print("An unexpected error occurred: {}".format(e))
--- podcast_model.py
+++ podcast_model.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
 """ models for category and podcasts """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Foreignkey
 from sqlachemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
-class Category():
+class Category(Base):
     """ podcast categories for the db"""
     __tablename = "category"
     id = Column(Integer, primary_key=True)
     name = Column(String(256), nullable=False)
 
-class Podcast():
+class Region(Base):
+    """podcast based on region for the db"""
+    __tablename__ = "Region"
+    id = Column(Integer, primary_key=True)
+    name = Column(string(65), nullable=False)
+
+class Country(Base):
+    """country table"""
+    __tablename__ = "country"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(65), nullable=False)
+    region_id = Column(Integer, ForeignKey(Region.id), nullable=False)
+
+class Podcast(Base):
     " podcast categories for the db"""
-    __tablename = "podcast_category"
+    __tablename = "podcast"
     id = Column(Integer, primary_key=True)
     name = Column(String(256), nullable=False)
     description = Column(String(256), nullable=False)
-    category_id = Column(Integer, nullable=False)
+    category_id = Column(Integer, ForeignKey(category.id), (nullable=False)
+    country_id = Column(Integer, ForeignKey(country.id), nullable=False)
+    region_id = Column(Integer, ForeignKey(Region.id), nullable=False)
+
  
    # id INT NOT NULL AUTO_INCREMENT,
    # name VARCHAR(256) NOT NULL,
