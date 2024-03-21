4c4b841d4f21b857b1a0f6e92c653c38501bc2cb
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-21 15:14:52 +0100

    Update __init__.py

--- yimbo_appli/__init__.py
+++ yimbo_appli/__init__.py
@@ -2,14 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from .views import yb
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '80ab528d4604e4d073b613216f6a0822'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///yimbo.db'
 
+app.register_blueprint(yb)
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
 from yimbo_appli import routes
+
+with app.app_context():
+    db.create_all()
