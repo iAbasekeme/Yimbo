3c312582605eeb71bc14131b9a1dc681491c499c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-21 15:14:12 +0100

    Update artists.py

--- models/artists.py
+++ models/artists.py
@@ -3,12 +3,12 @@ from . import db
 
 class Artists(db.Model):
     '''A artists model'''
-    __tablename__ = 'artists'
+    __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), nullable=False)
     about = db.Column(db.String(60), nullable=False)
-    # playlists = db.relationship('Playlist', back_populates="artist")
+    playlists = db.relationship('Playlist', back_populates="artist")
     albums = db.relationship('albums', backref="artist", lazy=True)
     tracks = db.relationship('Tracks', backref="artist", lazy=True)
     image_file = db.Column(db.String(20), nullable=False,
@@ -19,6 +19,17 @@ class Artists(db.Model):
     def __repr__(self):
         """Str representation"""
         return f"User's {self.name} {self.about}"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'about': self.about,
+            'Albums': self.albums,
+            'singles': self.tracks,
+            'image': self.image_file,
+            'Monthly_listeners': self.subscribers
+        }
 
 
 with app.app_context():
