f7787e44f334649aff96805b97d4c818794c3fa5
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-03-30 13:58:16 +0100

    Update musixmatch_api.py

--- musixmatch_api.py
+++ musixmatch_api.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import os
 from flask import jsonify
+from dotenv import load_dotenv
 
 MY_API_KEY = "8b45ee61f43e5fe313a0b788fe98c2f5"
 
@@ -24,11 +25,11 @@ def get_track():
         return None
     try:
         artist_country = artist_info['message']['body']['artist_country']
-        if artist_country and artist_country not in os.getenv(ACCEPTED_CODES):
+        if artist_country and artist_country not in os.getenv("ACCEPTED_CODES", "").split(","):
             print('Artist not from africa')
         else:
             print(artist_info)
     except (KeyError, AttributeError):
-        print("Error retrieving artist information or country data. Check API response structure.")
+        print("Error retrieving artist information or country data. Check API respons structure.")
 
 get_track()
